92c162babcfde07e3999c9c630bef5b1de3ec531
Muhammad Ari Al Ghifari <56450870+alfari24@users.noreply.github.com>
2026-04-07 14:14:18 +0700

    Allow CSRF bypass for Midtrans webhook route
    Fix error when Midtrans send post notification.
    
    Notification status : Failed
    Response : 
    > POST https://my.domain.id/extensions/midtrans/webhook
    < URL not found

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Paymenter\Extensions\Gateways\Midtrans\Midtrans;
 
-Route::post('/extensions/midtrans/webhook', [Midtrans::class, 'webhook'])->name('extensions.gateways.midtrans.webhook');
+Route::post('/extensions/midtrans/webhook', [Midtrans::class, 'webhook'])->withoutMiddleware([VerifyCsrfToken::class])->name('extensions.gateways.midtrans.webhook');
